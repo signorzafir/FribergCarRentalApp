63a66c6b35b213e9d3ae42d2426a3436f8b35369
Nayab Zafir <signorzafir@gmail.com>
2025-02-06 08:48:41 +0100

    Setting Up RentalAppDbContext Class

---
+++ Data/RentalAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Eventing.Reader;
+
+namespace FribergCarRentalApp.Data
+{
+    public class RentalAppDbContext : DbContext
+    {
+        public RentalAppDbContext(DbContextOptions<RentalAppDbContext> options) : base(options) { }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+    }
+}
